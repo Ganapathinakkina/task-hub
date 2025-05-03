23bb1939bee7f3930b5652c7da87a85ba83f9ce7
Ganapathinakkina <163103034+Ganapathinakkina@users.noreply.github.com>
2025-05-03 14:24:46 +0000

    Structurted responses are added to middleware

--- taskhub-backend/middlewares/auth.middleware.js
+++ taskhub-backend/middlewares/auth.middleware.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken');
+const { error } = require('../utils/response');
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized' });
 
+  if (!authHeader || !authHeader.startsWith('Bearer '))
+    return error(res, 'No token provided', 401);
+  
   const token = authHeader.split(' ')[1];
-  try {
+
+  try 
+  {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(403).json({ message: 'Forbidden' });
+    return error(res, 'Invalid or expired token', 401);
   }
 };
 
