1d84625c901eb0a93f6c3b6257eaf367784ee3ca
Ganapathinakkina <163103034+Ganapathinakkina@users.noreply.github.com>
2025-05-03 14:21:09 +0000

    missing imports were added

--- taskhub-backend/routes/auth.routes.js
+++ taskhub-backend/routes/auth.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
 const ROLES = require('../constants/roles');
+const { verifyToken, allowRoles } = require('../middlewares/auth.middleware');
 
 router.post('/register', register);
 router.post('/login', login);
 
-router.get('/protected', verifyToken, authorizeRoles(ROLES.ADMIN, ROLES.MANAGER), (req, res) => {
+router.get('/protected', verifyToken, allowRoles(ROLES.ADMIN, ROLES.MANAGER), (req, res) => {
     return success(res, `Hello ${req.user.role}, you're authorized to access this route`);
   });
 
